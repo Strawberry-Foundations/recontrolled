e68c5a9f56b3ca60deeec001187ad5d2c3cab527
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-23 16:45:06 +0100

    Moved colors.rs to vars/colors.rs

---
+++ src/vars/colors.rs
@@ -0,0 +1,19 @@
+pub const BOLD: &str = "\x1b[1m";
+pub const UNDERLINE: &str = "\x1b[4m";
+pub const C_RESET: &str = "\x1b[0m";
+// pub const BLACK: &str = "\x1b[30m";
+
+pub const RED: &str = "\x1b[31m";
+pub const GREEN: &str = "\x1b[32m";
+pub const YELLOW: &str = "\x1b[33m";
+pub const BLUE: &str = "\x1b[34m";
+pub const PURPLE: &str = "\x1b[35m";
+pub const CYAN: &str = "\x1b[36m";
+pub const WHITE: &str = "\x1b[37m";
+
+pub const LIGHT_GREEN: &str = "\x1b[92m";
+pub const LIGHT_BLUE: &str = "\x1b[94m";
+pub const LIGHT_MAGENTA: &str = "\x1b[95m";
+pub const LIGHT_CYAN: &str = "\x1b[96m";
+
+pub const DUMMY: &str = "";
