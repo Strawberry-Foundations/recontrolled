ca703e0b51f0965019f67eb2fa745a787785531e
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-23 16:57:33 +0100

    added VERSION & BUILD_DATE

--- src/vars/constants.rs
+++ src/vars/constants.rs
@@ -1,1 +1,4 @@
+pub const VERSION: &str = "3.0.0";
+pub const BUILD_DATE: &str = "2023-08-20";
+
 pub const RECONTROLLED_STR: &str = "\x1b[1m\x1b[96mR\x1b[32me\x1b[33mc\x1b[34mo\x1b[35mn\x1b[36mt\x1b[94mr\x1b[31mo\x1b[92ml\x1b[95ml\x1b[92me\x1b[33md\x1b[0m\x1b[1m\x1b[37m";
