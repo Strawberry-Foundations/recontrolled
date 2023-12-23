73e82cd258c9620cd6886b48c26afc822ed9cd89
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-23 20:54:41 +0100

    Added blink_switch command

---
+++ src/led/blink_switch.rs
@@ -0,0 +1,51 @@
+use std::fs::File;
+use std::io::Write;
+use std::thread;
+use std::time::Duration;
+use crate::vars::colors::{BOLD, GREEN, RED, WHITE, C_RESET, LIGHT_GREEN};
+use crate::vars::constants::{FILE_ACT_LED, FILE_PWR_LED};
+
+pub fn blink_switch(delay: u64) {
+    let blink_mode = match delay {
+        2000 => "Slow ",
+        1000 => "",
+        500 => "Fast ",
+        250 => "Super Fast ",
+        _ => "Custom "
+    };
+
+    println!("{BOLD}{WHITE}{blink_mode}Switching Blink mode for {RED}Power-{WHITE} and {LIGHT_GREEN}Activity-LED{WHITE} was {GREEN}activated{C_RESET}");
+
+    let mut pwr_led_file = File::create(FILE_PWR_LED).unwrap_or_else(|error| {
+        eprintln!("{}{}Error while opening {}: {}{}", BOLD, RED, FILE_PWR_LED, error, C_RESET);
+        std::process::exit(1);
+    });
+
+    let mut act_led_file = File::create(FILE_ACT_LED).unwrap_or_else(|error| {
+        eprintln!("{}{}Error while opening {}: {}{}", BOLD, RED, FILE_ACT_LED, error, C_RESET);
+        std::process::exit(1);
+    });
+
+
+    loop {
+        thread::sleep(Duration::from_millis(delay));
+        pwr_led_file.write_all("1".as_bytes()).unwrap_or_else(|error| {
+            eprintln!("{}{}Error while writing to {}: {}{}", BOLD, RED, FILE_PWR_LED, error, C_RESET);
+            std::process::exit(1);
+        });
+        act_led_file.write_all("0".as_bytes()).unwrap_or_else(|error| {
+            eprintln!("{}{}Error while writing to {}: {}{}", BOLD, RED, FILE_ACT_LED, error, C_RESET);
+            std::process::exit(1);
+        });
+
+        thread::sleep(Duration::from_millis(delay));
+        pwr_led_file.write_all("0".as_bytes()).unwrap_or_else(|error| {
+            eprintln!("{}{}Error while writing to {}: {}{}", BOLD, RED, FILE_PWR_LED, error, C_RESET);
+            std::process::exit(1);
+        });
+        act_led_file.write_all("1".as_bytes()).unwrap_or_else(|error| {
+            eprintln!("{}{}Error while writing to {}: {}{}", BOLD, RED, FILE_ACT_LED, error, C_RESET);
+            std::process::exit(1);
+        });
+    }
+}
