ab46ddb5a4b55bb252513741d1c0427a1ef77eec
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-23 16:16:24 +0100

    starting rust rewrite

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+
+mod help;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        help::help();
+        std::process::exit(0)
+    }
+
+    match args[1].as_str() {
+        "help" => help::help(),
+        _ => help::help()
+    }
+}
