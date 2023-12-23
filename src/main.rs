128132ab188ab42521e05dedd7feabfd544b150c
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-23 16:27:23 +0100

    Better code structure

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use std::env;
 
-mod help;
 mod colors;
+mod basics {
+    pub mod help;
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        help::help();
+        basics::help::print_help();
         std::process::exit(0)
     }
 
     match args[1].as_str() {
-        "help" => help::help(),
-        _ => help::help()
+        "help" => basics::help::print_help(),
+        _ => basics::help::print_help()
     }
 }
