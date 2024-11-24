8c6b0011e90859156cdeaea541f2efb49d9afbba
Juliandev02 <julian@strawberryfoundations.org>
2024-11-24 15:48:08 +0100

    fix: fix clippy warnings

--- src/led/basic_handler.rs
+++ src/led/basic_handler.rs
@@ -2,25 +2,25 @@ use std::fs::File;
 use std::io::Write;
 use crate::vars::colors::{BOLD, GREEN, RED, WHITE, C_RESET, LIGHT_GREEN};
 use crate::vars::constants::{FILE_ACT_LED, FILE_PWR_LED};
-use crate::vars::modules::{LED, Status};
+use crate::vars::modules::{Led, Status};
 
-pub fn set_status(led: LED, status: Status) {
+pub fn set_status(led: Led, status: Status) {
     let led_file_path = match led {
-        LED::PWR => FILE_PWR_LED,
-        LED::ACT => FILE_ACT_LED
+        Led::PWR => FILE_PWR_LED,
+        Led::ACT => FILE_ACT_LED
     };
 
     let led_string = match led {
-        LED::PWR => format!("{}Power-LED", RED),
-        LED::ACT => format!("{}Activity-LED", LIGHT_GREEN)
+        Led::PWR => format!("{}Power-LED", RED),
+        Led::ACT => format!("{}Activity-LED", LIGHT_GREEN)
     };
 
     let mut led_file = match led {
-        LED::PWR => File::create(FILE_PWR_LED).unwrap_or_else(|error| {
+        Led::PWR => File::create(FILE_PWR_LED).unwrap_or_else(|error| {
             eprintln!("{}{}Error while opening {}: {}{}", BOLD, RED, FILE_PWR_LED, error, C_RESET);
             std::process::exit(1);
         }),
-        LED::ACT => File::create(FILE_ACT_LED).unwrap_or_else(|error| {
+        Led::ACT => File::create(FILE_ACT_LED).unwrap_or_else(|error| {
             eprintln!("{}{}Error while opening {}: {}{}", BOLD, RED, FILE_ACT_LED, error, C_RESET);
             std::process::exit(1);
         })
