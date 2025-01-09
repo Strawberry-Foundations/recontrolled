99f16a30d77a174f1d52222dbd072b6154cc0b2f
Juliandev02 <julian@strawberryfoundations.org>
2025-01-09 20:50:27 +0100

    refactor: remove unused enums and improve panic_err! macro

--- src/utils/log.rs
+++ src/utils/log.rs
@@ -1,8 +1,8 @@
 #[macro_export]
 macro_rules! panic_err {
-    ($($arg:tt)*) => {
+    ($($arg:tt)*) => {{
         let res = std::fmt::format(format_args!($($arg)*));
         eprintln!("{}", res);
         std::process::exit(1);
-    }
+    }}
 }
