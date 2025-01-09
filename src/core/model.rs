6b77c073238792e10eadeaf076d7e0f97e300415
Juliandev02 <julian@strawberryfoundations.org>
2025-01-09 20:53:32 +0100

    feat: add function to retrieve Raspberry Pi model from cpuinfo

--- src/core/model.rs
+++ src/core/model.rs
@@ -3,6 +3,17 @@ use crate::core::led::{Led, Status};
 
 use std::io::Read;
 
+pub fn get_raspberry_pi_model() -> Option<String> {
+    use std::fs;
+
+    fs::read_to_string("/proc/cpuinfo").ok().and_then(|cpuinfo| {
+        cpuinfo
+            .lines()
+            .find(|line| line.starts_with("Model"))
+            .map(|line| line.replace("Model\t\t: ", ""))
+    })
+}
+
 #[derive(Debug)]
 pub enum RaspberryPiModel {
     PiZero2W,
