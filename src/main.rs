b4282b132dbe9e5aed89d5337de060ae663c5ce0
Juliandev02 <julian@strawberryfoundations.org>
2025-11-16 22:11:05 +0100

    feat: refactor LED control logic to use LEDController for improved structure and maintainability

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@
 
 use crate::constants::colors::{BOLD, C_RESET, RED};
 use crate::core::led::{Led, Status};
-use crate::core::model::{RaspberryPi, RaspberryPiModel, get_raspberry_pi_model};
+use crate::core::hardware::{RaspberryPi, RaspberryPiModel, get_raspberry_pi_model};
 use crate::core::rp02w::RaspberryPiZero2W;
 use crate::core::rp4::RaspberryPi4B;
 use crate::core::rp5::RaspberryPi5B;
+use crate::led::LEDController;
 
 use std::env;
-use std::env::consts::OS;
 
 mod basics;
 mod constants;
@@ -21,48 +21,17 @@ const FAST_BLINK_DELAY: u64 = 500;
 const SLOW_BLINK_DELAY: u64 = 2000;
 const SUPER_FAST_DELAY: u64 = 250;
 
-fn handle_custom_blink(args: &[String], led: Led, model: Box<dyn RaspberryPi>) {
-    if args.len() < 3 {
-        eprintln!("{RED}{BOLD}Invalid argument for blink delay{C_RESET}");
-        basics::help::print_help();
-        std::process::exit(1);
-    }
-
-    let delay = args[2].parse::<u64>().unwrap_or_else(|_| {
-        eprintln!("{RED}{BOLD}Invalid type for blink delay{C_RESET}");
-        std::process::exit(1);
-    });
-
-    led::blink::blink(led, delay, model);
-}
-
-fn handle_sync_mode(args: &[String], sync_fn: fn(u64, Box<dyn RaspberryPi>), model: Box<dyn RaspberryPi>) {
-    if args.len() < 3 {
-        sync_fn(DEFAULT_BLINK_DELAY, model);
-        return;
-    }
-
-    let delay = args[2].parse::<u64>().unwrap_or_else(|_| {
-        eprintln!("{RED}{BOLD}Invalid type for blink delay{C_RESET}");
-        std::process::exit(1);
-    });
-
-    sync_fn(delay, model);
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if !cfg!(target_os = "linux") {
-        panic_err!(
-            "{BOLD}{RED}Platform '{OS}' is not supported! Only Linux is supported!{C_RESET}"
-        );
+        panic_err!("{BOLD}{RED}Recontrolled does only support Linux / Raspberry Pi OS!{C_RESET}");
     }
 
-    let model: Box<dyn RaspberryPi> = match get_raspberry_pi_model()
+    let hardware: Box<dyn RaspberryPi> = match get_raspberry_pi_model()
         .and_then(|model| RaspberryPiModel::from_raw(&model))
         .unwrap_or_else(|| {
-            if env::args().collect::<Vec<String>>().contains(&String::from("--force")) {
+            if env::args().any(|x| x == "--force") {
                 return RaspberryPiModel::Pi4B;
             }
 
@@ -79,37 +48,86 @@ fn main() {
         std::process::exit(0);
     }
 
+    let controller = LEDController::new(hardware);
+
     match args[1].as_str() {
         // Basic Commands
-        "about" => basics::about::about(model),
+        "about" => basics::about::about(controller.hardware),
         "version" | "ver" => basics::version::version(),
-        "status" => basics::status::status(model),
+        "status" => basics::status::status(controller.hardware),
 
-        // LED Control
-        "ar" => led::basic_handler::set_status(Led::Pwr, Status::On, model),
-        "dr" => led::basic_handler::set_status(Led::Pwr, Status::Off, model),
-        "ag" => led::basic_handler::set_status(Led::Act, Status::On, model),
-        "dg" => led::basic_handler::set_status(Led::Act, Status::Off, model),
+        // LED Control for Power LED
+        "pwr" => match args[2].as_str() {
+            "on" => controller.set_status(Led::Pwr, Status::On),
+            "off" => controller.set_status(Led::Pwr, Status::Off),
+            "blink" => controller.blink(Led::Pwr, DEFAULT_BLINK_DELAY),
+            "blink_fast" => controller.blink(Led::Pwr, FAST_BLINK_DELAY),
+            "blink_slow" => controller.blink(Led::Pwr, SLOW_BLINK_DELAY),
+            "blink_super_fast" => controller.blink(Led::Pwr, SUPER_FAST_DELAY),
+            "blink_custom" => {
+                if args.len() < 4 {
+                    eprintln!("{RED}{BOLD}Invalid argument for blink delay{C_RESET}");
+                    basics::help::print_help();
+                    std::process::exit(1);
+                }
+                let delay = args[3].parse::<u64>().unwrap_or_else(|_| {
+                    eprintln!("{RED}{BOLD}Invalid type for blink delay{C_RESET}");
+                    std::process::exit(1);
+                });
+                controller.blink(Led::Pwr, delay);
+            }
+            _ => basics::help::print_help(),
+        },
 
-        // Blink modes for Power LED
-        "br" => led::blink::blink(Led::Pwr, DEFAULT_BLINK_DELAY, model),
-        "brf" => led::blink::blink(Led::Pwr, FAST_BLINK_DELAY, model),
-        "brs" => led::blink::blink(Led::Pwr, SLOW_BLINK_DELAY, model),
-        "brff" => led::blink::blink(Led::Pwr, SUPER_FAST_DELAY, model),
-        "brc" => handle_custom_blink(&args, Led::Pwr, model),
-
-        // Blink modes for Activity LED
-        "bg" => led::blink::blink(Led::Act, DEFAULT_BLINK_DELAY, model),
-        "bgf" => led::blink::blink(Led::Act, FAST_BLINK_DELAY, model),
-        "bgs" => led::blink::blink(Led::Act, SLOW_BLINK_DELAY, model),
-        "bgff" => led::blink::blink(Led::Act, SUPER_FAST_DELAY, model),
-        "bgc" => handle_custom_blink(&args, Led::Act, model),
+        "act" => match args[2].as_str() {
+            "on" => controller.set_status(Led::Act, Status::On),
+            "off" => controller.set_status(Led::Act, Status::Off),
+            "blink" => controller.blink(Led::Act, DEFAULT_BLINK_DELAY),
+            "blink_fast" => controller.blink(Led::Act, FAST_BLINK_DELAY),
+            "blink_slow" => controller.blink(Led::Act, SLOW_BLINK_DELAY),
+            "blink_super_fast" => controller.blink(Led::Act, SUPER_FAST_DELAY),
+            "blink_custom" => {
+                if args.len() < 4 {
+                    eprintln!("{RED}{BOLD}Invalid argument for blink delay{C_RESET}");
+                    basics::help::print_help();
+                    std::process::exit(1);
+                }
+                let delay = args[3].parse::<u64>().unwrap_or_else(|_| {
+                    eprintln!("{RED}{BOLD}Invalid type for blink delay{C_RESET}");
+                    std::process::exit(1);
+                });
+                controller.blink(Led::Act, delay);
+            }
+            _ => basics::help::print_help(),
+        },
 
         // Sync modes
-        "lb" => led::blink_sync::blink_sync(DEFAULT_BLINK_DELAY, model),
-        "lbs" => led::blink_switch::blink_switch(DEFAULT_BLINK_DELAY, model),
-        "lbc" => handle_sync_mode(&args, led::blink_sync::blink_sync, model),
-        "lbsc" => handle_sync_mode(&args, led::blink_switch::blink_switch, model),
+        "blink_sync" => controller.blink_sync(DEFAULT_BLINK_DELAY),
+        "blink_switch" => controller.blink_switch(DEFAULT_BLINK_DELAY),
+        "blink_sync_custom" => {
+            if args.len() < 3 {
+                eprintln!("{RED}{BOLD}Invalid argument for blink delay{C_RESET}");
+                basics::help::print_help();
+                std::process::exit(1);
+            }
+            let delay = args[2].parse::<u64>().unwrap_or_else(|_| {
+                eprintln!("{RED}{BOLD}Invalid type for blink delay{C_RESET}");
+                std::process::exit(1);
+            });
+            controller.blink_sync(delay);
+        }
+        "blink_switch_custom" => {
+            if args.len() < 3 {
+                eprintln!("{RED}{BOLD}Invalid argument for blink delay{C_RESET}");
+                basics::help::print_help();
+                std::process::exit(1);
+            }
+            let delay = args[2].parse::<u64>().unwrap_or_else(|_| {
+                eprintln!("{RED}{BOLD}Invalid type for blink delay{C_RESET}");
+                std::process::exit(1);
+            });
+            controller.blink_switch(delay);
+        }
         _ => basics::help::print_help(),
     }
 }
