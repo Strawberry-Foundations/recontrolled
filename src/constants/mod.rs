a637d746571e1145d27eb6573ba43bc620355ded
Juliandev02 <julian@strawberryfoundations.org>
2025-11-16 22:21:44 +0100

    chore: update BUILD_DATE

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -1,7 +1,7 @@
 pub mod colors;
 
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
-pub const BUILD_DATE: &str = "2025-01-09";
+pub const BUILD_DATE: &str = "2025-11-16";
 
 pub const FILE_PWR_LED: &str = "/sys/class/leds/PWR/brightness";
 pub const FILE_ACT_LED: &str = "/sys/class/leds/ACT/brightness";
